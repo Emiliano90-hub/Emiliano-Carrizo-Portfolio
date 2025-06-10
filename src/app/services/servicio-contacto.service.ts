967dd2d87c0299c560a9acf9c7808d1900cafb46
Emiliano <emilianocarrizo77@gmail.com>
2025-06-11 01:12:55 +0200

    Servicio modificado

--- src/app/services/servicio-contacto.service.ts
+++ src/app/services/servicio-contacto.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class ServicioContactoService {
 
-  private apiUrl = 'http://localhost/backend/enviar.php';
+  private apiUrl = 'https://emilianowebcontacto.infinityfreeapp.com/enviar.php';
   constructor(private http: HttpClient) { }
 
   enviarFormulario(data: any) {
