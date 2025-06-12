3e38d1b81a68d077bb714e3b486d769828720597
Emiliano-Carrizo <emilianocarrizo77@gmail.com>
2025-06-12 23:33:44 +0200

    Update servicio-contacto.service.ts

--- src/app/services/servicio-contacto.service.ts
+++ src/app/services/servicio-contacto.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class ServicioContactoService {
   private apiUrl = 'https://emilianowebcontacto.infinityfreeapp.com/enviar.php';
   constructor(private http: HttpClient) { }
 
-  enviarFormulario(data: any) {
+  enviarFormulario(data: any): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post(this.apiUrl, data, {headers, responseType: 'text'});
+    return this.http.post(this.apiUrl, JSON.stringify(data), {headers, responseType: 'text'});
   }
 }
