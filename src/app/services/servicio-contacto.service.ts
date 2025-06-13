26c1dad5bee750360382fc4d49c20473cdb7103f
Emiliano <emilianocarrizo77@gmail.com>
2025-06-13 14:18:09 +0200

    Actualizado el layout

--- src/app/services/servicio-contacto.service.ts
+++ src/app/services/servicio-contacto.service.ts
@@ -1,16 +1,17 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServicioContactoService {
 
-  private apiUrl = 'https://emilianowebcontacto.infinityfreeapp.com/enviar.php';
+  private apiUrl = 'https://backendcontacto.onrender.com/enviar.php';
   constructor(private http: HttpClient) { }
 
-  enviarFormulario(data: any) {
+  enviarFormulario(data: any): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post(this.apiUrl, data, {headers, responseType: 'text'});
+    return this.http.post(this.apiUrl, JSON.stringify(data), {headers, responseType: 'text'});
   }
 }
